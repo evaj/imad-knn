74da781272754dc717ca8678c0964782ca6d9751
Ewcia! <evajuralewicz@gmail.com>
2018-05-08 19:34:50 +0200

    test

--- src/data_loader.py
+++ src/data_loader.py
@@ -7,4 +7,3 @@ from settings import DATA_DIRECTORY
 def load_data(filename, attributes=None):
     data = pd.read_csv(os.path.join(DATA_DIRECTORY, filename) + '.data')
     return data if attributes is None else data[[attributes]]
-
