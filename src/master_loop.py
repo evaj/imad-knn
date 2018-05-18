3546f16304409860bbcf38fd0a31c6481a56cce1
Ewcia! <evajuralewicz@gmail.com>
2018-05-18 08:19:25 +0200

    master loop working

--- src/master_loop.py
+++ src/master_loop.py
@@ -9,52 +9,65 @@ import numpy as np
 MIN_ATTRIBUTES = 3
 
 
-def powerset(iterable):
+def powerset(iterable, min_attr):
     xs = list(iterable)
-    return chain.from_iterable(combinations(xs, n) for n in range(MIN_ATTRIBUTES, len(xs)+1))
+    return chain.from_iterable(combinations(xs, n) for n in range(min_attr, len(xs)+1))
 
 
-datasets = ['iris', 'wine', 'diabetes', 'glass', 'seeds']
-weights = ['uniform', 'distance']
+def custom_function(distances):
+    return list(reversed(np.argsort(distances).tolist()))
+
+
+datasets = {'iris': 3, 'wine': 9, 'diabetes': 5, 'glass': 6, 'seeds': 4}
+weights = ['uniform', 'distance', custom_function]
 metrics = ['minkowski', 'euclidean', 'manhattan']
 
-result_frame = pd.DataFrame(columns=["K", "Metric", "Dataset", "Attributes", "Voting",
-                                     "Accuracy", "F-score", "Precision", "Recall"])
-for dataset in datasets:
+result_frame = pd.DataFrame(columns=["K", "Metric", "Dataset", "Voting", "CV",
+                                     "Accuracy","F-score", "Precision", "Recall"])
+for dataset, min_attr in datasets.items():
     print('DATASET ' + dataset)
     data = data_loader.load_data(dataset)
+    attr_columns = [col for col in data.columns if col != 'Type' and col != 'Id']
+    train_data = data[attr_columns]
+    labels = data['Type']
     if dataset == 'iris':
-        data['Type'] = data['Type'].astype('category').cat.codes
-    column_combinations = list(powerset([column for column in data.columns if column != 'Type']))
+        labels = labels.astype('category').cat.codes
+    labels = labels.as_matrix()
+    train_data = (train_data - train_data.mean()) / (train_data.max() - train_data.min())
+    column_combinations = list(powerset(train_data.columns, len(train_data.columns)))
     data_length = data.shape[0]
-    # k_values = max(1, int(0.005 * data.shape[0]))
-    # noinspection PyTypeChecker
-    # k_values = [k_values] + [int(number) for number in range(1, int(data.shape[0]/data['Type'].max()))]
     k_values = np.arange(3, 30, 2)
     for combination in column_combinations:
         for weight in weights:
             for metric in metrics:
                 for k in k_values:
-                    accuracies = []
-                    recalls = []
-                    fscores = []
-                    precisions = []
-                    for train, test in StratifiedKFold(n_splits=5, shuffle=True).split(data, data['Type']):
-                        clf = neighbors.KNeighborsClassifier(k, weights=weight, metric=metric)
-                        print('K: ' + str(k) + " Combinations: " + str(list(combination)))
-                        sub_data = data[list(combination)].as_matrix()
-                        labels = data[['Type']].as_matrix()
-                        clf.fit(sub_data[train], labels[train])
-                        predictions = clf.predict(sub_data[test.tolist()])
-                        accuracies.append(accuracy_score(labels[test], predictions))
-                        precisions.append(precision_score(labels[test], predictions, average="macro"))
-                        recalls.append(recall_score(labels[test], predictions, average="macro"))
-                        fscores.append(f1_score(labels[test], predictions, average="macro"))
-                    result = {'K': k, "Metric": metric, "Dataset": dataset, "Voting": weights,
-                              "Attributes": list(combination),
-                              'Accuracy': np.mean(accuracies),
-                              'Recall': np.mean(recalls),
-                              'F-score': np.mean(fscores),
-                              'Precision': np.mean(precisions)}
-                    result_frame = result_frame.append(result, ignore_index=True)
-                result_frame.to_csv("results.csv", index=False)
+                    for cv in range(2, 7):
+                        accuracies = []
+                        recalls = []
+                        fscores = []
+                        precisions = []
+                        for train, test in StratifiedKFold(n_splits=cv, shuffle=True).split(train_data, labels):
+                            clf = neighbors.KNeighborsClassifier(k, weights=weight, metric=metric)
+                            print('K: ' + str(k) + " Combinations: " + str(list(combination)))
+                            sub_data = train_data[list(combination)].as_matrix()
+                            # labels = labels.as_matrix()
+                            clf.fit(sub_data[train], labels[train])
+                            predictions = clf.predict(sub_data[test.tolist()])
+                            accuracies.append(accuracy_score(labels[test], predictions))
+                            precisions.append(precision_score(labels[test], predictions, average="macro"))
+                            recalls.append(recall_score(labels[test], predictions, average="macro"))
+                            fscores.append(f1_score(labels[test], predictions, average="macro"))
+                        if callable(weight):
+                            weight_name = 'reverse_distance'
+                        else:
+                            weight_name = weight
+                        result = {'K': k, "Metric": metric, "Dataset": dataset,
+                                  "Voting": weight_name,
+                                  "CV": cv,
+                                  # "Attributes": list(combination),
+                                  'Accuracy': np.mean(accuracies),
+                                  'Recall': np.mean(recalls),
+                                  'F-score': np.mean(fscores),
+                                  'Precision': np.mean(precisions)}
+                        result_frame = result_frame.append(result, ignore_index=True)
+                    result_frame.to_csv("results.csv", index=False)
